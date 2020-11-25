5be0f303d68c8a022ab861592e7b1420bedf93d3
philanderson888 <philanderson888@hotmail.com>
2020-11-25 16:18:57 +0000

    Messing around with cookies on react front end

--- projects/jwt/jwt/back-end-https-3/server.js
+++ projects/jwt/jwt/back-end-https-3/server.js
@@ -4,6 +4,7 @@ const https = require('https');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const Cookies = require('cookies');
 const cors = require('cors');
 const privateKey = fs.readFileSync('key.pem');
 const publicCertificate = fs.readFileSync('cert.pem');
@@ -16,9 +17,10 @@ const jsonwebtoken = require('jsonwebtoken');
 const jwtSecret = 'secret123';
 const jwtExpirySeconds = 100;
 const app = express();
+app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json())
-app.use(cookieParser());
+const keys = ['keyboard cat'];
 const homePage = (request,response) => {
     response.setHeader('Access-Control-Allow-Origin','*');
     response.setHeader('Access-Control-Request-Method','*');
@@ -27,6 +29,12 @@ const homePage = (request,response) => {
     const data = {
         helloworld:"you are now receiving data over https"
     }
+    console.log(`cookies in request object from react client`,request.cookies)
+    console.log(`cookies in response object being sent to react client`,response.cookies)
+    
+    const cookies = new Cookies(request,response,{keys});
+    const lastVisit = cookies.get('LastVisit');
+    cookies.set('LastVisit',new Date().toISOString());
     response.end(JSON.stringify(data));
 }
 const jwt = (request, response) => {
@@ -77,28 +85,56 @@ const signin = (request,response) => {
         response.send('not authorised')
         return response.status(401).end()
     }
-    response.setHeader('Access-Control-Allow-Origin','*');
+    // response.setHeader('Access-Control-Allow-Origin','*');
+    response.setHeader('Access-Control-Allow-Origin','http://localhost:3000');
     response.setHeader('Access-Control-Request-Method','*');
     response.setHeader('Access-Control-Allow-Methods','OPTIONS,GET')
     const token = jsonwebtoken.sign({ user }, jwtSecret, {
         algorithm: 'HS256',
         expiresIn: jwtExpirySeconds,
     })
-    response.cookie('token',token,{maxAge: jwtExpirySeconds * 1000})
+    response.cookie('token',token,{maxAge: jwtExpirySeconds * 1000, sameSite:'lax'});
+    response.cookie('fromTheServer','here is a cookie from the server',{maxAge:200});
+    const cookies = new Cookies(request,response,{keys});
+    const lastVisit = cookies.get('LastVisit');
+    console.log('lastVisit',lastVisit);
+    cookies.set('LastVisit',new Date().toISOString());
     response.writeHead(200,{'Content-Type':'application/json'});
     console.log(`token sent as cookie at ${new Date()}`,token)
+    console.log(`cookies in request object from react client`,JSON.stringify(request.cookies))
+    console.log(`cookies in response object being sent to react client`,JSON.stringify(response.cookies));
     response.end(JSON.stringify(userData))
 }
+
 app.get('/',homePage);
 app.get('/jwt',jwt);
 app.get('/users',users);
-app.get('/foods',foods);
 app.post("/signin", signin)
+app.get('/foods',foods);
+// app.use('/',expressJwt({
+//     secret:jwtSecret,
+//     algorithms:['HS256'],
+// }));
 // foods now requires a token
 app.use('/',expressJwt({
     secret:jwtSecret,
     algorithms:['HS256'],
-}))
+    getToken: function fromCookie(request) {
+        const token = request.cookies.token || request.body.token
+        console.log(`trying to get a token in the application root path`,token)
+        if(token) {
+            return token;
+        }
+        return null;
+    }
+}).unless({
+    path:[
+        '/',
+        '/jwt',
+        '/signin',
+        '/users',
+    ]
+}));
 const httpServer = http.createServer(app)
 const httpsServer = https.createServer(credentials,app)
 httpServer.listen(3001);
